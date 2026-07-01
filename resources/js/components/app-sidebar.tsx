28db23362f08a5ad4d8a79fae9238df16fba0552
Mogaya <mogayainnocent@gmail.com>
2026-07-01 23:50:26 +0300

    Updated sidebar navigation items for consistency and added stock levels entry

--- resources/js/components/app-sidebar.tsx
+++ resources/js/components/app-sidebar.tsx
@@ -4,7 +4,7 @@ import { NavUser } from '@/components/nav-user';
 import { Sidebar, SidebarContent, SidebarFooter, SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from '@/components/ui/sidebar';
 import { type NavItem } from '@/types';
 import { Link } from '@inertiajs/react';
-import { LayoutGrid, Package } from 'lucide-react';
+import { Boxes, LayoutGrid, Package } from 'lucide-react';
 import AppLogo from './app-logo';
 
 const mainNavItems: NavItem[] = [
@@ -14,9 +14,15 @@ const mainNavItems: NavItem[] = [
         icon: LayoutGrid,
     },
     {
-        title: 'My Products',
+        title: 'My products',
         url: '/business/products',
         icon: Package,
+    },
+
+    {
+        title: 'Stock levels',
+        url: '/business/stock',
+        icon: Boxes,
     },
 ];
 
